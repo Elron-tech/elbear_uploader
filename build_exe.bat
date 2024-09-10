29d4fa93b46279ee3a213617805a7b769f666174
khristolyubov <khristolyubov@elron.tech>
2024-09-10 16:19:22 +0700

    папки для сборки

--- build_exe.bat
+++ build_exe.bat
@@ -1,5 +1,5 @@
 python -m nuitka .\elbear_uploader.py ^
---output-dir=build ^
+--output-dir=build_win ^
 --output-filename="elbear_uploader.exe" ^
 --standalone
 
--- build_linux.sh
+++ build_linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 python -m nuitka ./elbear_uploader.py \
---output-dir=build \
+--output-dir=build_linux \
 --output-filename="elbear_uploader" \
 --standalone
 
-cd build
+cd build_linux
 mv elbear_uploader.dist elbear_uploader
 tar -czvf elbear_uploader_linux.tar.gz elbear_uploader
